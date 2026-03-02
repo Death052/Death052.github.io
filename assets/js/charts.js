646a630069f008fd2070ab50073bc6ba4698bdfa
Yik-Hei-Kwok <63249970+Death052@users.noreply.github.com>
2026-03-02 20:57:07 +0000

    Added new stuff

---
+++ assets/js/charts.js
@@ -0,0 +1,48 @@
+const yValues = ["C/C++", "C#", "Python", "HTML/JS", "Java"];
+const xValues = [55, 75, 90, 60, 65];
+const barColors = ['#97CFA8', '#97CFA8','#97CFA8','#97CFA8','#97CFA8'];
+const borderColor = ['#6B9E62', '#6B9E62','#6B9E62','#6B9E62','#6B9E62'];
+
+const ctx = document.getElementById('Coding_Language_Chart').getContext('2d');
+
+Chart.defaults.font.family = "VT323";
+Chart.defaults.font.size = 18;
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: yValues,
+    datasets: [{
+      label: 'Familiarity (%)',
+      backgroundColor: barColors,
+      borderColor: borderColor,
+      borderWidth: 2,
+      data: xValues,
+      barThickness: 20,
+      maxBarLength: 100
+    }]
+  },
+  
+  options: {
+
+    indexAxis: 'y',
+    scales: {
+      x: {
+        min: 0,
+        max: 100 
+      }
+    },
+
+
+    plugins: {
+      legend: {
+        labels: {
+          // This more specific font property overrides the global property
+          font: {
+              size: 18
+          }
+        }
+      }
+    }
+  }
+});
