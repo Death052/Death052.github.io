edfeda26970fd9bcbda4916e053d08707869cf96
Yik-Hei-Kwok <63249970+Death052@users.noreply.github.com>
2026-03-06 12:01:51 +0000

    ...

--- assets/js/switch_lang.js
+++ assets/js/switch_lang.js
@@ -60,7 +60,7 @@ function onChangePageClick(event, targetPage, currentPage) {
         // check if going back to index
         if (targetPage === 'index.html#three'){
             targetPage = '#three';
-             console.log("targetPage is correct");
+            console.log("targetPage is correct");
         }else{
             console.log("targetPage is ", targetPage);
         }
@@ -69,15 +69,13 @@ function onChangePageClick(event, targetPage, currentPage) {
         const separator = base.endsWith('/') ? '' : '/';
 
         if (lang === 1) {
-            window.location.href = base + separator + targetPage;
+            //window.location.href = base + separator + targetPage;
         }else if (lang === 2) {
-            window.location.href = base + separator + targetPage;
+            //window.location.href = base + separator + targetPage;
         }else if (lang === 3) {
-            window.location.href = base + separator + targetPage;
+           //window.location.href = base + separator + targetPage;
         }
     }
-        }
-	}
 
 
 // function onChangePageClick(event, targetPage, currentPage) {
